acfdfbb938b93551a139f1ea5e712a70845cc333
Feerraz <guilhermeferraz2000@gmail.com>
2025-11-25 22:07:37 -0300

    Horas para minutos

--- src/pages/hourtomin.tsx
+++ src/pages/hourtomin.tsx
@@ -1,64 +1,81 @@
-import MenuPage from '@/pages/mintohour'
+import {
+  useEffect,
+  useState
+} from 'react'
+import {
+  Button,
+  Flex
+} from 'antd'
 import { MaskedInput } from 'antd-imask-input'
-import { useState } from 'react'
-import { Minutes } from '@/types/minutes'
+
+// contexts
+import { useTheme } from '@/contexts/theme'
+import {
+  OperationType,
+  useResults
+} from '@/contexts/results'
+
+
+// components
+import PageHeader from '@/components/Header'
 
 function Index() {
 
-  const [ value, setValue ] = useState( '' )
-  const [ result, setResult ] = useState<Minutes>( [] )
+  const { setHeader } = useTheme()
+  const { addResult } = useResults()
+  const [
+    value,
+    setValue
+  ] = useState<string>( '' )
 
-  const calcular = ( value: string ) => {
+
+  useEffect( () => {
+    setHeader( () => <PageHeader
+      title={ 'Horas para Minutos' }
+    /> )
+  }, [] )
+
+  function handleCalculate() {
+
+    if ( !value.includes( ':' ) ) return
+
     const hor = value.split( ':' )
-
     const minutos = ( ( parseInt( hor[ 0 ] ) * 60 ) + ( parseInt( hor[ 1 ] ) ) ).toString()
 
-    if ( value == '' ) {
-      return
-    }
-
-    return (
-      setResult( result => [ ...result,
-      {
-        minutos: minutos,
-        qtdHour: value
-      } ] )
-
-    )
+    addResult( {
+      op: OperationType.hourtomin,
+      res: String( minutos ),
+      a: value
+    } )
+    setValue( () => '' )
   }
 
+
   return (
-    <div>
-      <MenuPage />
-      <div style={ { marginLeft: '256px', padding: '20px' } }>
-        <h1 className='titulo-gradiente'>Horas para minutos</h1>
-        <label style={ { display: 'block', marginBottom: '4px' } }>
-          <MaskedInput
-            mask={ '000:00' }
-            maskOptions={ {
-              lazy: true
-            } }
-            placeholder='Digite a quantidade de horas'
-            size='large'
-            value={ value.padStart( 1, '0' ) }
-            onChange={ e => setValue( e.target.value ) }
-            onKeyDown={ e => {
-              if ( e.key === 'Enter' || e.key === 'Tab' ) {
-                calcular( value )
-              }
-            } }
-          />
-        </label>
-        <button className='button-calculate' onClick={ () => calcular( value ) }>Calcular</button>
-        <div className='divMinuteToHour'> Resultados:
-          <ul style={ { listStyleType: 'none' } }>
-            { result.map( ( item, index ) => (
-              <li key={ index }>{ item.qtdHour } - { item.minutos } minutos</li>
-            ) ) }
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Flex
+      orientation='vertical'
+      gap='middle'
+    >
+      <MaskedInput
+        allowClear
+        onPressEnter={ handleCalculate }
+        maskOptions={ {
+          mask: '000:00',
+          lazy: true
+        } }
+        placeholder='Horas (hh:mm)'
+        size='large'
+        value={ value }
+        onChange={ ( { maskedValue } ) => setValue( () => maskedValue ) }
+      />
+
+      <Button
+        type='primary'
+        onClick={ handleCalculate }
+      >
+        Calcular
+      </Button>
+    </Flex>
   )
 }
 
